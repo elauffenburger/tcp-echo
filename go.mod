00cd23a9a29c8997a00ba91c89ffaecde295f46d
Eric Lauffenburger <elauffenburger@gmail.com>
2022-09-29 13:49:06 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/elauffenburger/tcp-echo
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	l := log.New(os.Stdout, "[tcp-echo] ", log.Flags())
+
+	addr := os.Getenv("TCP_ECHO_ADDR")
+	if addr == "" {
+		addr = ":"
+	}
+
+	srv, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	l.Printf("listening on %s\n", srv.Addr())
+
+	for {
+		conn, err := srv.Accept()
+		if err != nil {
+			l.Printf("error accepting connection: %s\n", err)
+			continue
+		}
+
+		l.Println("received connection")
+
+		go func() {
+			rdr := bufio.NewReader(conn)
+
+			str, err := rdr.ReadString('\n')
+			if err != nil {
+				l.Printf("error reading payload: %s\n", err)
+				return
+			}
+
+			l.Printf("%s", str)
+
+			n, err := io.Copy(conn, strings.NewReader(str))
+			if err != nil {
+				l.Printf("error writing payload: %s\n", err)
+				return
+			}
+
+			if n != int64(len(str)) {
+				l.Printf("only wrote %d/%d bytes\n", n, len(str))
+				return
+			}
+
+			if err := conn.Close(); err != nil {
+				l.Printf("error closing conn: %s\n", err)
+				return
+			}
+		}()
+	}
+}
