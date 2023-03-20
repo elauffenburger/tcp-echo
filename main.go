4d3607f644987b601462e3209d395d84365832e1
Eric Lauffenburger <elauffenburger@gmail.com>
2023-03-20 14:33:35 -0700

    add http health srv

--- main.go
+++ main.go
@@ -2,73 +2,126 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"strings"
 )
 
 func main() {
 	l := log.New(os.Stdout, "[tcp-echo] ", log.Flags())
+	ctx := context.Background()
 
 	addr := os.Getenv("TCP_ECHO_ADDR")
 	if addr == "" {
 		addr = ":"
 	}
 
-	srv, err := net.Listen("tcp", addr)
+	fixedResponse := os.Getenv("TCP_ECHO_RESPONSE")
+
+	srv := newServer(addr, &fixedResponse, l)
+	srv.listenAndServe(ctx)
+}
+
+type server struct {
+	l *log.Logger
+
+	tcpAddr          string
+	tcpFixedResponse *string
+}
+
+func newServer(tcpAddr string, tcpFixedResponse *string, l *log.Logger) *server {
+	return &server{l, tcpAddr, tcpFixedResponse}
+}
+
+func (s *server) listenAndServe(ctx context.Context) {
+	go s.startTCP(ctx)
+	go s.startHTTP(ctx)
+
+	<-ctx.Done()
+}
+
+func (s *server) startTCP(_ context.Context) {
+	srv, err := net.Listen("tcp", s.tcpAddr)
 	if err != nil {
 		panic(err)
 	}
 
-	fixedResponse := os.Getenv("TCP_ECHO_RESPONSE")
-
-	l.Printf("listening on %s\n", srv.Addr())
+	s.l.Printf("TCP listening on %s\n", srv.Addr())
 
 	for {
 		conn, err := srv.Accept()
 		if err != nil {
-			l.Printf("error accepting connection: %s\n", err)
+			s.l.Printf("error accepting connection: %s\n", err)
 			continue
 		}
 
-		l.Println("received connection")
+		s.l.Println("received connection")
 
 		go func() {
 			rdr := bufio.NewReader(conn)
 
 			str, err := rdr.ReadString('\n')
 			if err != nil && !errors.Is(err, io.EOF) {
-				l.Printf("error reading payload: %s\n", err)
+				s.l.Printf("error reading payload: %s\n", err)
 				return
 			}
 
-			l.Printf("%s", str)
+			s.l.Printf("%s", str)
 
 			var response string
-			if fixedResponse != "" {
-				response = fixedResponse
+			if s.tcpFixedResponse != nil && *s.tcpFixedResponse != "" {
+				response = *s.tcpFixedResponse
 			} else {
 				response = str
 			}
 
 			n, err := io.Copy(conn, strings.NewReader(response))
 			if err != nil {
-				l.Printf("error writing payload: %s\n", err)
+				s.l.Printf("error writing payload: %s\n", err)
 				return
 			}
 
 			if n != int64(len(response)) {
-				l.Printf("only wrote %d/%d bytes\n", n, len(response))
+				s.l.Printf("only wrote %d/%d bytes\n", n, len(response))
 				return
 			}
 
 			if err := conn.Close(); err != nil {
-				l.Printf("error closing conn: %s\n", err)
+				s.l.Printf("error closing conn: %s\n", err)
 				return
 			}
 		}()
 	}
+}
+
+func (s *server) startHTTP(_ context.Context) {
+	srv := http.NewServeMux()
+	srv.Handle("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}))
+
+	addr := s.httpAddr()
+	s.l.Printf("http listening on %s", addr)
+
+	http.ListenAndServe(addr, srv)
+}
+
+func (s *server) httpAddr() string {
+	addrParts := strings.SplitN(s.tcpAddr, ":", 2)
+
+	var host string
+	if len(addrParts) > 0 {
+		host = addrParts[0]
+	} else {
+		host = ""
+	}
+
+	return fmt.Sprintf("%s:80", host)
 }
